988f97f6449b08f63a2c318d5828e49954689b46
chuan1983 <chuan1983126@gmail.com>
2016-09-20 09:25:45 +0800

    v2

--- app/src/main/java/org/iii/tw/asynctest/MainActivity.java
+++ app/src/main/java/org/iii/tw/asynctest/MainActivity.java
@@ -1,8 +1,11 @@
 package org.iii.tw.asynctest;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View v){
+
+    }
+    private class MyTask extends AsyncTask<Void,Void,Void>{             //非同步的概念
+        @Override
+        protected Void doInBackground(Void... params) {                //在背景中去做
+            Log.d("brad","doInBackground");
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {        //前
+            super.onPreExecute();
+            Log.d("brad","onPreExecute");
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {   //後
+            super.onPostExecute(aVoid);
+            Log.d("brad","onPostExecute");
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {  //更新
+            super.onProgressUpdate(values);
+            Log.d("brad","onProgressUpdate");
+
+        }
+
+        @Override
+        protected void onCancelled(Void aVoid) {        //結束
+            super.onCancelled(aVoid);
+            Log.d("brad","onCancelled");
+
+        }
     }
 }
